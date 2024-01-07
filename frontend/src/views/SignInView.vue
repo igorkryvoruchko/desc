a5c441a90cb6fce7e0063a25119384659f3bde7f
IgorKryvoruchko <igor.kryvoruchko.88@gmail.com>
2024-01-07 13:58:27 +0100

    frontend

---
+++ frontend/src/views/SignInView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="sign_in">
+    <h1>Sign In</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "SignInView",
+};
+</script>
---
+++ frontend/src/views/SignUpView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="sign_up">
+    <h1>Sign Up</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "HomeView",
+};
+</script>
