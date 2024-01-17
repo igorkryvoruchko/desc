96400c411de152c138508ed3ea24b00655003273
IgorKryvoruchko <igor.kryvoruchko.88@gmail.com>
2024-01-18 00:00:42 +0100

    fix front with docker and fix sign out

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,13 +9,15 @@
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
         <v-toolbar-title
-          ><router-link to="/">Desc</router-link></v-toolbar-title
+          ><router-link to="/" class="home-link"
+            >Desc</router-link
+          ></v-toolbar-title
         >
 
         <v-spacer></v-spacer>
-
-        <v-btn icon>
-          <v-icon>mdi-export</v-icon>
+        <h3 class="home-link">{{ this.user.name }}</h3>
+        <v-btn v-if="this.user.name" @click="signOutAction" class="home-link">
+          Sign Out
         </v-btn>
       </v-toolbar>
       <router-view />
@@ -24,11 +26,26 @@
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
 export default {
   name: "App",
 
   data: () => ({
     //
   }),
+  computed: {
+    ...mapState({
+      user: "user",
+    }),
+  },
+  methods: {
+    ...mapActions(["signOutAction"]),
+  },
 };
 </script>
+
+<style>
+.home-link {
+  color: antiquewhite;
+}
+</style>
